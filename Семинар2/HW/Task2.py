ff39cd145b83638cc096bdd7458d2066d61b7429
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-10-31 22:20:09 +0300

    S2HWTask2

--- Семинар2/HW/Task2.py
+++ Семинар2/HW/Task2.py
@@ -1,7 +1,12 @@
 #Задача 2. Выведите таблицу истинности 
 # для выражения ¬(X ∧ Y) ∨ Z.
 
-N = int(input('Введите число'))
-numbers = []
-for i in range(N):
-    numbers.append(-N, N+1)
+x = [0, 1]
+y = [0, 1]
+z = [0, 1]
+print("x| y| z| ¬(X ∧ Y) ∨ Z")
+for x in range(0 , 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            sum = not x and y or z
+            print(f"{x}| {y}| {z}| {int (sum)}")
