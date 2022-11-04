b0e7153e9a792670bbbb7d9014147815aac9301c
Evgeniy <108060138+Newet7@users.noreply.github.com>
2022-11-04 17:51:49 +0300

    S3Task1

---
+++ Семинар3/HW/task1/task1.py
@@ -0,0 +1,22 @@
+#Задача 1. Создайте файл. Запишите в него N первых 
+# элементов последовательности Фибоначчи.
+#6 –> 1 1 2 3 5 8
+#path = 'file.txt'
+data = open('file.txt', 'w')
+
+fib1 = 0
+fib2 = 1
+n = int(input('Введите число: '))
+
+file = [fib1, fib2]
+
+i = 0
+while i < n - 2:
+    fib_sum = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib_sum
+    i = i + 1
+    print(fib_sum)
+    file.append(fib_sum)
+print(file)
+data.writelines(str(file))
